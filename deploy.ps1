f0b3c56001b4cd858640380d8e99ff904961bf1f
Wanno Drijfhout <github.com@wanno.eu>
2021-07-31 22:14:50 +0200

    Don't deploy .git folder

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ $to = "root@10.69.1.10"
 # Note that deleting everything will also delete SECRETS
 #ssh $to "rm -rf ${dirName}"
 
-scp -r ${dirPath} ${to}:
+scp -r "${dirPath}/[!.]*" "${to}:"
 #scp -r ${dir}\hosts ${to}:/etc/ansible/hosts
 #ssh -A $to "ansible-playbook -i inventories/experimental ${dirName}/site.yml"
 #ssh -A $to "cd ${dirName} && ansible-playbook -i inventories/stable site.yml"
