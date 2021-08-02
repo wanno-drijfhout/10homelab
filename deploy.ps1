79c471c2f95ab7cf878190343becc2691e72471c
Wanno Drijfhout <github.com@wanno.eu>
2021-08-02 19:44:54 +0200

    Make script executable again
    Previous linting seems to have introduced regressions

--- deploy.ps1
+++ deploy.ps1
@@ -1,15 +1,15 @@
 $dirPath = ($PSCommandPath | Split-Path -Parent).ToString()
 $dirName = ($dirPath | Split-Path -Leaf).ToString()
-$to = "root@10.69.1.10"
+$to = "root@10.69.1.10:~/${dirName}"
 
 # Note that deleting everything will also delete SECRETS
 #ssh $to "rm -rf ${dirName}"
 
-scp -r "${dirPath}/[!.]*" "${to}:"
+scp -r "${dirPath}/[!.]*" "${to}"
 #scp -r ${dir}\hosts ${to}:/etc/ansible/hosts
 #ssh -A $to "ansible-playbook -i inventories/experimental ${dirName}/site.yml"
 #ssh -A $to "cd ${dirName} && ansible-playbook -i inventories/stable site.yml"
 echo "
 Run remotely:
-    ${to}:~/${dirName}# /usr/local/bin/ansible-playbook site.yml
+    ${to}# /usr/local/bin/ansible-playbook site.yml
 "
